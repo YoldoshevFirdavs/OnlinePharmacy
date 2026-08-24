8f787e2f6f0c9fda28c593bf7e24589110774548
yoldoshevpydev-crypto <yoldoshevfpydev@gmail.com>
2026-08-25 00:50:58 +0500

    fix: format code with black and isort

--- pharmacy/models/__init__.py
+++ pharmacy/models/__init__.py
@@ -2,15 +2,7 @@ from .comments import CommentAnalysis, CommentLike, ProductComment
 from .feedback import Feedback
 from .history import CustomerUserHistory
 from .medicine import Category, Medicine
-from .misc import (
-    BotInlineButton,
-    BotMenuStep,
-    FlashSale,
-    MedicineImage,
-    ProductViewHistory,
-    Review,
-    StockLog,
-)
+from .misc import BotInlineButton, BotMenuStep, FlashSale, MedicineImage, ProductViewHistory, Review, StockLog
 
 __all__ = [
     "Medicine",
